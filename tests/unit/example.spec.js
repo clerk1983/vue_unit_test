1002000b1096e9f38df554a72b9b9d590ac695e9
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2022-12-25 20:04:55 +0900

    feat(コンポーネントテスト追加):

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,16 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import {mount} from '@vue/test-utils'
+import App from '@/components/App'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
 
-it("two plus is four", () => {
-  expect(2 + 2).toBe(4);
-});
+
+test("Test App Component", () => {
+  const wrapper = mount(App)
+  expect(wrapper.text()).toBe('Hello World')
+})
